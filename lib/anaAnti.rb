0909b6561ce23a488542ae51616bc2ce6c92d347
Tae Lee <taebumlee@gmail.com>
2019-07-19 14:56:11 -0700

    This update for refactoring.

--- lib/anaAnti.rb
+++ lib/anaAnti.rb
@@ -17,14 +17,24 @@ class Anagram
     message3 = "These words have no letter matches and are antigrams."
 
     if (first_word_array.any? {|vowel| vowel_array.include?(vowel)}) & (second_word_array.any? {|vowel| vowel_array.include?(vowel)}) == false then return @message = message1 end
-    # return keyword in here ignore following lines and end up action of function.
-    first_word_array.each { |char| second_word_array.include?(char) ? @message = message2 : @message = message3 }
+      first_word_array.each { |char| second_word_array.include?(char) ? @message = message2 : @message = message3 }
     @message
   end
 end
 
-# first_word_array.each() do |char|
-#   if second_word_array.include?(char) then @message = message2 else @message = message3 end
+
+# def comparison()
+#   first_word_array = @word1.delete('^A-Za-z').downcase.split(//)
+#   second_word_array = @word2.delete('^A-Za-z').downcase.split(//)
+#   vowel_array = ["a", "i", "e", "o", "u"]
+#   message1 = "You need to input actual words!"
+#   message2 = "These words are anagrams."
+#   message3 = "These words have no letter matches and are antigrams."
+#
+#   if (first_word_array.any? {|vowel| vowel_array.include?(vowel)}) & (second_word_array.any? {|vowel| vowel_array.include?(vowel)}) == false then return @message = message1 end
+#   # return keyword in here ignore following lines and end up action of function.
+#   first_word_array.each { |char| second_word_array.include?(char) ? @message = message2 : @message = message3 }
+#   @message
 # end
 
 #   def comparison()
