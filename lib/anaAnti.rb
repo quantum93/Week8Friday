4d305ef8cc68bacb5b2562405a678bf66a5a2d67
Tae Lee <taebumlee@gmail.com>
2019-07-22 23:55:55 -0700

    This is test for 4th message.

--- lib/anaAnti.rb
+++ lib/anaAnti.rb
@@ -18,22 +18,21 @@ class Anagram
     vowel_in_second = second_word_array.any? do |vowel|
       vowel_array.include?(vowel)
     end
-    # vowel_in_first = first_word_array.any? {|vowel| vowel_array.include?(vowel)}
-    # vowel_in_second = second_word_array.any? {|vowel| vowel_array.include?(vowel)}
     message1 = "You need to input actual words!"
     message2 = "These words are anagrams."
     message3 = "These words have no letter matches and are antigrams."
+    message4 = "These words are neithor anagrams nor antigrams, but the real words."
 
     if vowel_in_first & vowel_in_second == false
       return @message = message1
     end
-      first_word_array.each do |char|
-        if second_word_array.include?(char)
-           @message = message2
-         else
-           @message = message3
-         end
-       end
+    first_word_array.each do |char|
+      if second_word_array.include?(char)
+        @message = message2
+      else
+        @message = message3
+      end
+    end
     @message
   end
 end
--- spec/anaAnti_spec.rb
+++ spec/anaAnti_spec.rb
@@ -7,48 +7,44 @@ describe('anagram logic for two words') do
   #   words = Anagram.new('ruby', 'bury')
   #   expect(words.comparison()).to(eq(["r", "u", "b", "y"]["b", "u", "r", "y"]))
   # end
-  it('gives "These words are anagrams." for ruby, bury') do
+  it('gives These words are anagrams for ruby, bury') do
     words = Anagram.new('ruby', 'bury')
     expect(words.comparison()).to(eq("These words are anagrams."))
   end
-  it('gives "These words are anagrams." for rubxxy, bury') do
+  it('gives These words are anagrams for rubxxy, bury') do
     words = Anagram.new('ruby', 'bury')
     expect(words.comparison()).to(eq("These words are anagrams."))
   end
-  it('gives "These words are anagrams." for Tea, Eat') do
+  it('gives These words are anagrams for Tea, Eat') do
     words = Anagram.new('Tea', 'Eat')
     expect(words.comparison()).to(eq("These words are anagrams."))
   end
-  it('gives "These words are anagrams." for cccc, bury') do
+  it('gives These words are anagrams for cccc, bury') do
     words = Anagram.new('cccc', 'ruby')
     expect(words.comparison()).to(eq("You need to input actual words!"))
   end
-  it('gives "These words are anagrams." for bcdf, ruby') do
+  it('gives These words are anagrams for bcdf, ruby') do
     words = Anagram.new('bcdf', 'ruby')
     expect(words.comparison()).to(eq("You need to input actual words!"))
   end
-  it('gives "These words are anagrams." for ruby, bcdf') do
+  it('gives These words are anagrams for ruby, bcdf') do
     words = Anagram.new('ruby', 'bcdf')
     expect(words.comparison()).to(eq("You need to input actual words!"))
   end
-  it('gives "These words are anagrams." for hi, bye') do
+  it('gives These words are anagrams for hi, bye') do
     words = Anagram.new('hi', 'bye')
     expect(words.comparison()).to(eq("These words have no letter matches and are antigrams."))
   end
-  it('gives "These words are anagrams." for hi?, b ye ') do
+  it('gives These words are anagrams for hi?, b ye ') do
     words = Anagram.new('hi?', 'b ye ')
     expect(words.comparison()).to(eq("These words have no letter matches and are antigrams."))
   end
-  it('gives "These words are anagrams." for ruby&, @bury') do
+  it('gives These words are anagrams for ruby&, @bury') do
     words = Anagram.new('ruby&', '@bury ')
     expect(words.comparison()).to(eq("These words are anagrams."))
   end
-  it('gives These phrases are anagrams. for ruby apple, bury orange') do
-    words = Anagram.new("ruby apple", "bury orange")
-    expect(words.comparison()).to(eq("These words are anagrams."))
-  end
-  it('gives These phrases are anagrams. for ruby apple, bury orange') do
-    words = Anagram.new("ruby apple", "bury orange")
-    expect(words.comparison()).to(eq("These words are anagrams."))
+  it('gives These words are anagrams for button, puppy') do
+    words = Anagram.new("button", "puppy")
+    expect(words.comparison()).to(eq("These words have no letter matches and are antigrams."))
   end
 end
